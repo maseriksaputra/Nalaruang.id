c018ff0330f0706fc5efa5f1597f74e5c562d46c
Erika Dwi Saputra <saputrastd@gmail.com>
2026-06-17 18:33:07 +0700

    Force HTTPS scheme dynamically based on hostname

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (str_contains(request()->getHost(), 'nalaruang.id')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
