0cee2edd0dbfe77ff863765d5c1c4245a66d8e8e
Erika Dwi Saputra <saputrastd@gmail.com>
2026-06-17 19:09:34 +0700

    Add explicit livewire config

---
+++ config/livewire.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'asset_url' => env('APP_URL', 'https://nalaruang.id'),
+    'app_url' => env('APP_URL', 'https://nalaruang.id'),
+    'inject_assets' => true,
+    'navigate' => [
+        'show_progress_bar' => true,
+        'restore_scroll_position' => true,
+    ],
+];
