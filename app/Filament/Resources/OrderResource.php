d3725f90a778e09e6a77da8fd312e002b0dbf366
Erika Dwi Saputra <saputrastd@gmail.com>
2026-07-01 00:47:20 +0700

    fix: ensure empty Custom VIP schemas populate on Edit page via afterStateHydrated

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -125,6 +125,25 @@ class OrderResource extends Resource
                                 ['field_name' => 'Musik / Backsound (Opsional)', 'type' => 'audio', 'is_required' => false],
                                 ['field_name' => 'Request Desain Spesifik / Catatan', 'type' => 'textarea', 'is_required' => false],
                             ])
+                            ->afterStateHydrated(function (Forms\Components\Repeater $component, $state) {
+                                if (empty($state)) {
+                                    $component->state([
+                                        ['field_name' => 'Nama Mempelai Pria (Lengkap)', 'type' => 'text', 'is_required' => true],
+                                        ['field_name' => 'Nama Panggilan Mempelai Pria', 'type' => 'text', 'is_required' => true],
+                                        ['field_name' => 'Nama Orang Tua Mempelai Pria & Anak Keberapa', 'type' => 'textarea', 'is_required' => false],
+                                        ['field_name' => 'Nama Mempelai Wanita (Lengkap)', 'type' => 'text', 'is_required' => true],
+                                        ['field_name' => 'Nama Panggilan Mempelai Wanita', 'type' => 'text', 'is_required' => true],
+                                        ['field_name' => 'Nama Orang Tua Mempelai Wanita & Anak Keberapa', 'type' => 'textarea', 'is_required' => false],
+                                        ['field_name' => 'Informasi Akad Nikah (Tanggal, Jam, Tempat)', 'type' => 'textarea', 'is_required' => false],
+                                        ['field_name' => 'Informasi Resepsi (Tanggal, Jam, Tempat)', 'type' => 'textarea', 'is_required' => false],
+                                        ['field_name' => 'Informasi Rekening / Amplop Digital', 'type' => 'textarea', 'is_required' => false],
+                                        ['field_name' => 'Kisah Cinta / Narasi / Ucapan', 'type' => 'textarea', 'is_required' => false],
+                                        ['field_name' => 'Galeri Foto & Video', 'type' => 'image', 'is_required' => false, 'max_files' => 50],
+                                        ['field_name' => 'Musik / Backsound (Opsional)', 'type' => 'audio', 'is_required' => false],
+                                        ['field_name' => 'Request Desain Spesifik / Catatan', 'type' => 'textarea', 'is_required' => false],
+                                    ]);
+                                }
+                            })
                     ])
                     ->visible(function (\Filament\Forms\Get $get) {
                         $packageId = $get('package_id');
